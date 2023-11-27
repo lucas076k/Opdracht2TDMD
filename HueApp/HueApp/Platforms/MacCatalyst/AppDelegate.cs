5d14e6fc9ec2d0eab39dfd0d529cf0d1a28f0c3c
Lucas Kuijsters <94112834+lucas076k@users.noreply.github.com>
2023-11-27 14:39:47 +0100

    Delete HueApp directory
    Challas

--- HueApp/HueApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HueApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}

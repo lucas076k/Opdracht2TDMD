0c9b0c98b2a230f1efb0fc555ff62586e590df6c
Lucas Kuijsters <94112834+lucas076k@users.noreply.github.com>
2023-11-27 14:00:43 +0100

    Proper project added
    Ik kom rustig die oss binnen

---
+++ HueApp/HueApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HueApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ HueApp/HueApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HueApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
